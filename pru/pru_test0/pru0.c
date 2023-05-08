14f9f6e8ec66d80937520c30860f09ddcadf1151
Nguyen Hong An <an23593@gmail.com>
2023-05-08 22:45:12 +0700

    worked led blink

--- pru/pru_test0/pru0.c
+++ pru/pru_test0/pru0.c
@@ -2,19 +2,16 @@
  
 
 #define CYCLES_PER_SECOND 200000000 /* 200MHz clock */
-#define PR1_PRU0_PRU_R30_2 (1 << 2)
+#define PR1_PRU0_PRU_R30_2 (1 << 2) /* R30_2 */
  
-
 volatile register uint32_t __R30;   /* GPIO output register */
  
-
 void main(void) {
+
     while (1) {
-        /* Switch the GPIO with one cycle every millisecond.  This can be
-         * verified with an oscilloscope. */
         __R30 |= PR1_PRU0_PRU_R30_2;
-        __delay_cycles(CYCLES_PER_SECOND / 2000);
+        __delay_cycles(CYCLES_PER_SECOND * 2);
         __R30 &= ~PR1_PRU0_PRU_R30_2;
-        __delay_cycles(CYCLES_PER_SECOND / 2000);
+        __delay_cycles(CYCLES_PER_SECOND * 2);
     }
 }
